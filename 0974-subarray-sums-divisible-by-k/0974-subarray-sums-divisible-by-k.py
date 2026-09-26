1a8bfcde0fbe8d56e4f3d1548b0b9887245e4ae0
Aryan Kumar Pandey <aryankumarpandey00@gmail.com>
2026-09-26 19:03:28 +0530

    Time: 27 ms (87.03%), Space: 23.5 MB (54.04%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,20 +1,22 @@
 class Solution:
     def subarraysDivByK(self, nums: list[int], k: int) -> int:
         f = {}
-        sum  = 0
+        sum = 0
         result = 0
-        f[0] =1
+
+        f[0] = 1  # remainder 0 exists once initially
 
         for i in range(len(nums)):
-
             sum += nums[i]
-            rem = sum % k
+
+            rem = sum % k  # find remainder
+
             if rem < 0:
-                rem = rem+k
+                rem += k  # make remainder positive
 
-            freq = f.get(rem,0)
-            result += freq
+            freq = f.get(rem, 0)  # same remainder seen before 
+            result += freq  # each gives a divisible subarray
 
-            f[rem] = f.get(rem,0)+1
+            f[rem] = f.get(rem, 0) + 1  # store remainder in hashmap
 
-        return result 
+        return result
